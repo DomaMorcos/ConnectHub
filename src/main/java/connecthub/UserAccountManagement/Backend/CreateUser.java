87293bef963f9aa592b17d4fece97a67771b5aeb
DomaMorcos <domadios.morcos@gmail.com>
2024-12-04 14:50:45 +0200

    User , Content and Profile combined

--- src/main/java/connecthub/UserAccountManagement/Backend/CreateUser.java
+++ src/main/java/connecthub/UserAccountManagement/Backend/CreateUser.java
@@ -1,36 +1,35 @@
 package connecthub.UserAccountManagement.Backend;
 
 
+import java.util.ArrayList;
+
 import static connecthub.UserAccountManagement.Backend.HashPassword.hashPassword;
 import static connecthub.UserAccountManagement.Backend.Validation.isEmailValid;
 import static connecthub.UserAccountManagement.Backend.Validation.isUsernameValid;
 
 public class CreateUser {
-    private static long counter = 1;
-
-    public static synchronized String generateID() {
-        return String.valueOf(counter++);
-    }
-
     UserDatabase userDB = UserDatabase.getInstance();
 
+    public  synchronized String generateID() {
+        if (userDB.users == null) {
+            userDB.users = new ArrayList<>();
+        }
+        return Integer.toString(userDB.users.size() + 1);
+    }
+
+
+
     public boolean signup(String email, String username, String password, String dateOfBirth) {
-<<<<<<< HEAD
-        if (!isEmailValid(email) || !isUsernameValid(username) || userDB.contains(email)) {
-            return false;
-=======
 
         if (!isEmailValid(email) || !isUsernameValid(username) || userDB.contains(email)) {
             return false;
-
->>>>>>> 5af115e246cee752a3c80e7b46db847070cc315a
         }
         String hash = hashPassword(password);
         User newUser = new User(generateID(), email, username, hash, dateOfBirth, "offline");
         userDB.users.add(newUser);
-        userDB.printUsers();
         userDB.saveUsersToJsonFile();
         return true;
     }
 }
+
 
