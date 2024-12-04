c9f6312bcae53bc9e419883c557cd7e50e3777f2
Raphy-Takla <raphytakla@gmail.com>
2024-12-04 02:33:30 +0200

    Modified in ContentDatabase & GetContent

--- src/main/java/connecthub/ContentCreation/Backend/ContentDatabase.java
+++ src/main/java/connecthub/ContentCreation/Backend/ContentDatabase.java
@@ -1,22 +1,14 @@
 package connecthub.ContentCreation.Backend;
 
-import connecthub.UserAccountManagement.Backend.User;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
-import javax.json.JsonWriter;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class ContentDatabase {
     private static final String CONTENT_FILEPATH = "content.json";
@@ -28,6 +20,7 @@ public class ContentDatabase {
     }
 
     public static ContentDatabase getInstance() {
+        //only one instance
         if (contentDatabase == null) {
             contentDatabase = new ContentDatabase();
         }
@@ -36,16 +29,14 @@ public class ContentDatabase {
 
     public static void saveContents() {
         //loop on contents and make them json object
-
-        JSONArray contentsArray = new JSONArray();
+        JSONArray jsonArray = new JSONArray();
         for (Content content : contents) {
-            contentsArray.put(content.toJson());
+            jsonArray.put(content.toJson());
         }
-        //array list of json object
         //write in json file
         try {
             FileWriter file = new FileWriter(CONTENT_FILEPATH);
-            file.write(contentsArray.toString(4));
+            file.write(jsonArray.toString(4));
             file.close();
         } catch (IOException e) {
             System.out.println("Error while saving contents");
@@ -55,23 +46,20 @@ public class ContentDatabase {
     public static ArrayList<Content> loadContents() {
         // Clear the old one
         contents.clear();
-        // jsonReader for reading from json file
         try {
-            Path contentFile = Paths.get(CONTENT_FILEPATH);
-
-            // If the file doesn't exist, create it
-            if (!Files.exists(contentFile)) {
-                Files.createFile(contentFile); // Create an empty file
-                return contents; // Return empty list if no content exists yet
+            Path pathFile = Paths.get(CONTENT_FILEPATH);
+            // check the file exist
+            if (!Files.exists(pathFile)) {
+                Files.createFile(pathFile);
+                return contents;
             }
-            // Read the content of the file
-            String json = new String(Files.readAllBytes(contentFile));
-            JSONArray contentsArray = new JSONArray(json);
+            // read the file
+            String json = new String(Files.readAllBytes(pathFile));
+            //make jsonArray
+            JSONArray jsonArray = new JSONArray(json);
             // Loop on every json object
-            for (int i = 0; i < contentsArray.length(); i++) {
-                // Check for type
-                JSONObject jsonObject = contentsArray.getJSONObject(i);
-
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
                 // If post add to the contents a new post
                 if (jsonObject.getString("type").equals("post")) {
                     contents.add(Post.readFromJson(jsonObject));
@@ -82,7 +70,6 @@ public class ContentDatabase {
             }
             // After loading all remove expired
             removeExpiredStories();
-
             return contents;
         } catch (IOException e) {
             e.printStackTrace();
@@ -98,6 +85,7 @@ public class ContentDatabase {
     }
 
     public static synchronized int generateId() {
+        //the unique id is the place of content
         if (contents == null) {
             contents = new ArrayList<>();
         }
